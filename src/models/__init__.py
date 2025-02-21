b149381cd477e840f476659c1b57c8221757e443
ankush_013 <agang@sertiscorp.com>
2025-02-21 15:37:12 +0700

    fix: fix import for BaseReasoningModel

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,3 @@
-from src.models.base_model import BaseModel
+from src.models.base_model import BaseReasoningModel
 from src.models.language_model import LanguageReasoningModel
 from src.models.embedder import EmbeddingModel
